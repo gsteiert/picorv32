ee54388da8fa1a992c1a63f4fc1a1384d4acc3f6
Greg Steiert <greg.steiert@latticesemi.com>
2019-04-12 11:03:31 -0700

    added efb.h header file with EFB register definitions, moved pwm to D1

--- femtosoc/firmware/main.c
+++ femtosoc/firmware/main.c
@@ -1,17 +1,15 @@
 // simple downcounter for femtosoc
 
 #include <stdint.h>
+#include "efb.h"
+
+#define WB_BASE   0x04000000
+#define EFB_BASE  (WB_BASE + 0x0)
+#define EFB       ((efb_t *) EFB_BASE)
 
 #define reg_leds (*(volatile uint32_t*)0x01000000)
 #define DELAY_CNT 1000
 
-#define TCCR0 (*(volatile uint32_t*)0x04000178)
-#define TCCR1 (*(volatile uint32_t*)0x0400017C)
-#define TCTOPSET0 (*(volatile uint32_t*)0x04000180)
-#define TCTOPSET1 (*(volatile uint32_t*)0x04000184)
-#define TCOCRSET0 (*(volatile uint32_t*)0x04000188)
-#define TCOCRSET1 (*(volatile uint32_t*)0x0400018C)
-#define TCCR2 (*(volatile uint32_t*)0x04000190)
 
 
 void delay_ms(int32_t ms) {
@@ -26,18 +24,12 @@ void delay_ms(int32_t ms) {
 }
 
 void main() {
-    // TCTOPSET0 = 0x00;
-    // TCTOPSET1 = 0x01;
-    // TCOCRSET0 = 0x10;
-    // TCOCRSET1 = 0x00;
-    // TCCR0 = 0x22;
-    // TCCR1 = 0x1A;
     delay_ms(1000);
     int32_t i = -1;
     while(1) {
         i -= 1;
-        reg_leds = i >> 7;
-        TCOCRSET0 = i;        
+        reg_leds = i >> 8;
+        EFB->TCOCRSET[0] = i;        
         delay_ms(5);
     }
 }
